fb858859d0f0167a327aaa7bd4213062e7605f22
Jinhoe <kimjh620@naver.com>
2024-12-10 21:24:11 +0900

    store, customer 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoreModule } from './store/store.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [StoreModule, CustomerModule],
 })
 export class AppModule {}
